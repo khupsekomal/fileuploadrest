f3d9d159ff64fe3e91ba87ba4549ea2312469a83
Komal Kh <khupsekomal@gmail.com>
2022-09-24 23:52:07 +0530

    First commit Signed-off-by: Komal Kh <khupsekomal@gmail.com>

---
+++ src/main/java/com/intrvwqsns/FileuploadrestApplication.java
@@ -0,0 +1,13 @@
+package com.intrvwqsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intrvwqsns/FileuploadrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intrvwqsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
